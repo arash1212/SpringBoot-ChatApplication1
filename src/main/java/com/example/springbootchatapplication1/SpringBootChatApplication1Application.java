4f796fa46e2b2e34987c80039d857aa976ce6978
Arash <arashsalehi849@yahoo.com>
2023-07-03 11:35:55 +0330

    Initial Commit

---
+++ src/main/java/com/example/springbootchatapplication1/SpringBootChatApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootchatapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatApplication1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChatApplication1Application.class, args);
+    }
+
+}
